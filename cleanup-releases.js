b23d1ba88f93d3877b2bbc1e4bb898d3b563d966
2091k <140614254+2091k@users.noreply.github.com>
2024-09-01 13:39:02 +0800

    Update cleanup-releases.js

--- cleanup-releases.js
+++ cleanup-releases.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+const FILES_TOKEN = process.env.FILES_TOKEN;  // 更新为你设置的密钥名称
 const REPO_OWNER = '你的GitHub用户名';
 const REPO_NAME = '你的仓库名称';
 const RELEASE_ID = 172917597; // 你指定的Release ID
@@ -13,7 +13,7 @@ const deleteOldFiles = async () => {
     // 获取指定Release的详细信息
     const releaseDetails = await axios.get(`https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/releases/${RELEASE_ID}`, {
       headers: {
-        Authorization: `token ${GITHUB_TOKEN}`
+        Authorization: `token ${FILES_TOKEN}`
       }
     });
 
@@ -22,7 +22,7 @@ const deleteOldFiles = async () => {
       if (uploadTime < threeHoursAgo) {
         await axios.delete(asset.url, {
           headers: {
-            Authorization: `token ${GITHUB_TOKEN}`,
+            Authorization: `token ${FILES_TOKEN}`,
             'Accept': 'application/vnd.github.v3+json'
           }
         });
